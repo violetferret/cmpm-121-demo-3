b0d6bd8a9ef29d3da21791f618812d47c9b17939
violetferret <kahlstro@ucsc.edu>
2024-11-04 14:38:18 -0800

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 // todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "My Cool Game";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+const button  = document.createElement("button");
+button.innerHTML = "Click me!";
+app.append(button);
+
+button.addEventListener("click", function() {
+    alert("You clicked me! :o");
+})
